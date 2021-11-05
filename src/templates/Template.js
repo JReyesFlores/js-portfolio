b7b88a0bb61c90c05246c13c2e3e91e3c06a635c
jhon.phileppe.reyes.flores <jhon.reyes@openserver.com.pe>
2021-11-04 20:19:53 -0500

    Configurando la migración de archivos sin copyplugin

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
 import getData from '../utils/getData.js';
+import githubImg from '../assets/images/github.png';
+import twitterImg from '../assets/images/twitter.png';
+import instagramImg from '../assets/images/instagram.png';
 
 const Template = async () => {
   const data = await getData();
@@ -24,13 +27,13 @@ const Template = async () => {
         </div>
         <div class="card_social">
           <a href="https://twitter.com/gndx">
-            <img src="assets/images/twitter.png" />
+            <img src="${twitterImg}" />
           </a>
           <a href="https://github.com/gndx">
-            <img src="assets/images/github.png" />
+            <img src="${githubImg}" />
           </a>
           <a href="https://instagram.com/gndx">
-            <img src="assets/images/instagram.png" />
+            <img src="${instagramImg}" />
           </a>
         </div>
       </div>
