429f9e6f58afca78bed3eaee1c20c97d3f17251d
jhon.phileppe.reyes.flores <jhon.reyes@openserver.com.pe>
2021-11-07 02:44:42 -0500

    Agregando variables de entorno - .env

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,14 +1,14 @@
-const API = 'https://randomuser.me/api/';
+const defaulAPI = process.env.API;
 
 const getData = async (id) => {
-  const apiURl = id ? `${API}${id}` : API;
+  const apiURl = id ? `${defaulAPI}${id}` : defaulAPI;
   try {
     const response = await fetch(apiURl);
     const data = await response.json();
     return data.results[0];
   } catch (error) {
     console.log('Fetch Error', error);
-  };
+  }
 };
 
 export default getData;
